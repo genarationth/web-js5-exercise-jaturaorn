e10f38cf5c0eeecb2d0f08d35d5508b8402776ff
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2023-03-15 14:48:12 +0000

    do Exercise #3.2

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ const people = ['Sofia', 'David', 'Juan'];
 // people.shift();
 // console.log(people);
 //Do part3
-const addmid = people.splice(2,0,'Renata','Elena');
-console.log(addmid);
-console.log(people);
+// const addmid = people.splice(2,0,'Renata','Elena');
+// console.log(addmid);
+// console.log(people);
+// let temp = '';
+// for(let count = 1 ; count < 5; count++){
+//     temp += '*'
+//     console.log(temp);
+// }
+
+for (let i=1; i<=5; i++){
+    let temp = ''
+    for (let j=1; j<=i; j++){
+        temp += ''
+    }
+    console.log(temp)
+}
