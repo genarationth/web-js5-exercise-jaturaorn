b5808045d4811f1b465b94798b3e3aa4e0435de6
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2023-03-15 12:32:30 +0000

    do Exercise #1,Part 3

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const people = ['Sofia', 'David', 'Juan'];
 //FIFO
-people.push('Sara','Augustin');
-people.shift();
+// people.push('Sara','Augustin');
+// people.shift();
+// console.log(people);
+//Do part3
+const addmid = people.splice(2,0,'Renata','Elena');
+console.log(addmid);
 console.log(people);
